185b3c236c0ebfc4720dbe03a9a6fa782875bacb
Alextgn500 <azubkov500@gmail.com>
2024-08-13 19:10:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-name = 'Alexander'
-print( name )
-age = 68
-print(age)
-age += 1
-print(age)
-is_student = True
-print(is_student)
+my_string=('product')
+print('7:',len(my_string))
+print('product:',my_string.upper())
+print('product:',my_string.lower())
+print('все товары проданы'.replace('все товары проданы','все''товары#проданы'))
+print('первый символ строки:',my_string[0])
+print('последний символ строки',my_string[-1])
+
+
