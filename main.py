3108b98dd3cd0f3a4a0643be3f828916905ac912
Alextgn500 <azubkov500@gmail.com>
2024-08-14 13:57:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-my_string=('product')
-print('7:',len(my_string))
-print('product:',my_string.upper())
-print('product:',my_string.lower())
-print('все товары проданы'.replace('все товары проданы','все''товары#проданы'))
-print('первый символ строки:',my_string[0])
-print('последний символ строки',my_string[-1])
-
-
+immutable_var=(1,'Hello',0.125, True)
+print('tuple_immutable_var:',immutable_var)
+#immutable_var[0]=2
+#print(immutable_var)
+mutable_list = [1,2,3,4,5]
+mutable_list[0]=55
+mutable_list.append('59')
+mutable_list.remove(2)
+print(mutable_list)
