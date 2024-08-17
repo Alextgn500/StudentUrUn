609fc396dc7a0bb1add6d8bcd3b922a3e6341a56
Alextgn500 <azubkov500@gmail.com>
2024-08-17 16:15:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-print("Hi","PyCharm")
-x = 43
-y = 32
-print(x*y)
-print('End line')
+
+# Создаем телефонную книгу
+numbers = [1234567, 1234568, 1234569, 1234570, 1234571]
+names = {'Jonny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+# Сортируем имена в алфавитном порядке
+names_list = sorted(names)
+
+# Создаем словарь, связывающий имя и номер телефона
+phone_book = {}
+for i, name in enumerate(names_list):
+    phone_book[name] = numbers[i]
+
+# Выводим телефонную книгу
+for name, number in phone_book.items():
+    print(f"{name}: {number}")
+
